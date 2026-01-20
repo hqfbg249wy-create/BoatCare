eeb336859f2e4a89a8fee945e0e91cffa0948d9e
Ekkehart Padberg <e.padberg@padberg-beratung.de>
2026-01-20 17:14:10 +0100

    Initial Commit

---
+++ BoatCareTests/BoatCareTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoatCareTests.swift
+//  BoatCareTests
+//
+//  Created by Ekkehart Padberg on 20.01.26.
+//
+
+import Testing
+
+struct BoatCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
